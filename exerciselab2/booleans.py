0095ca32ce8c7be1e50bce2beb7ca51b1d1a1822
Shadiyar00 <shadiyar.sainov@gmail.com>
2024-02-09 02:46:29 +0600

    add exercise lab2

---
+++ exerciselab2/booleans.py
@@ -0,0 +1,19 @@
+#exercise 1
+print(10>9)
+#boolean value = "True"
+
+#exercise 2
+print(10 == 9)
+#boolean value = "False"
+
+#exercise 3
+print(10<9)
+#boolean value = "False"
+
+#exercise 4
+print(bool("abc"))
+#boolean value = "True"
+
+#exercise 5
+print(bool(0))
+#boolean value = "False"    
