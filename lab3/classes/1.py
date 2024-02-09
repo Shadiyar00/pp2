35f94005a771b463427d7996fc7bad00cb129d14
Shadiyar00 <shadiyar.sainov@gmail.com>
2024-02-10 01:33:49 +0600

    lab3

---
+++ lab3/classes/1.py
@@ -0,0 +1,11 @@
+class uppercase:
+    def __init__ (self):
+        self.input_string = ""
+        
+
+    def getString(self):
+        self.input_string = input()
+        
+    def printstring(self):
+        print(self.input_string.upper())
+
